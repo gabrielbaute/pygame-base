147686620c92a108bbae8a1b009294086d0a6758
gabrielbaute <gabrielbaute@gmail.com>
2026-02-12 07:40:28 -0400

    Incluimos los modulos en init para facilitar su importación como paquete

--- game/managers/__init__.py
+++ game/managers/__init__.py
@@ -2,3 +2,4 @@ from game.managers.asset_manager_error import AssetManagerError
 from game.managers.image_manager import ImageManager
 from game.managers.sound_manager import SoundManager
 from game.managers.scene_manager import SceneManager
+from game.managers.font_manager import FontManager
